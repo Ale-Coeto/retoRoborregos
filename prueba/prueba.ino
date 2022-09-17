71c7d1b344aa297b81d9accd690a162663e9c303
AlbertoA01285213 <a01285213@tec.mx>
2022-09-17 14:39:07 -0500

    Codigo V1

--- prueba/prueba.ino
+++ prueba/prueba.ino
@@ -1,10 +1,51 @@
+int motor_i_adelante = 2;
+int motor_i_atras = 3;
+
+// Motor derecho
+int motor_d_adelante = 4;
+int motor_d_atras = 5;
+
 void setup() {
-  // put your setup code here, to run once:
-//hola
-//hola2
+  // Motores
+  pinMode(motor_i_adelante, OUTPUT);
+  pinMode(motor_i_atras, OUTPUT);
+  pinMode(motor_d_adelante, OUTPUT);
+  pinMode(motor_d_atras, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 
+}
+
+void adelante()
+{
+  digitalWrite(motor_i_adelante, HIGH);
+  digitalWrite(motor_i_atras, LOW);
+  digitalWrite(motor_d_adelante, HIGH);
+  digitalWrite(motor_d_atras, LOW);
+}
+
+void atras()
+{
+  digitalWrite(motor_i_adelante, LOW);
+  digitalWrite(motor_i_atras, HIGH);
+  digitalWrite(motor_d_adelante, LOW);
+  digitalWrite(motor_d_atras, HIGH);
+}
+
+void izquierda()
+{
+  digitalWrite(motor_i_adelante, LOW);
+  digitalWrite(motor_i_atras, HIGH);
+  digitalWrite(motor_d_adelante, HIGH);
+  digitalWrite(motor_d_atras, LOW);
+}
+
+void derecha()
+{
+  digitalWrite(motor_i_adelante, HIGH);
+  digitalWrite(motor_i_atras, LOW);
+  digitalWrite(motor_d_adelante, LOW);
+  digitalWrite(motor_d_atras, HIGH);
 }
